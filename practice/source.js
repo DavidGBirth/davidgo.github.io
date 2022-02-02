8b7b169f58dbdeceb6b916d7f76072a2c7d91737
David <davidg.nascimento@hotmail.com>
2022-02-02 09:35:38 -0300

    practice

---
+++ practice/source.js
@@ -0,0 +1,18 @@
+//Generate a random number
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+//Sort alphabetically
+function(a, b){
+    if(a[type] < b[type]) { return -1; }
+    if(a[type] > b[type]) { return 1; }
+    return 0;
+});
+
+//Deal with dates
+let moment = venc.split("-");
+return new Date(moment[0], moment[1] - 1, moment[2]);
+
+//Get selection option
+let value = select.options[select.selectedIndex].text;
